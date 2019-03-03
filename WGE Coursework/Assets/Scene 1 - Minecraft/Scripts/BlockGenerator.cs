5ec8a411066eb5b64b09ee80812bd29b327d5df1
Ruari026 <36458213+Ruari026@users.noreply.github.com>
2019-03-03 01:25:23 +0000

    Initial Commit

---
+++ WGE Coursework/Assets/Scene 1 - Minecraft/Scripts/BlockGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockGenerator : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerController.OnPlaceBlock += PlaceBlock;
+
+        PlayerController.OnDestroyBlock += DestroyBlock;
+    }
+
+
+    /*
+    ==================================================
+    Mesh Interaction
+    ==================================================
+    */
+    private void PlaceBlock(Vector3 position)
+    {
+        Debug.Log("Placing Block At: " + position);
+    }
+
+    private void DestroyBlock()
+    {
+        Debug.Log("Destroying Block");
+    }
+}
